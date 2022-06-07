bb4ee6945a3124e6e73a1588a93274656f6653b7
m-bakula <m.bakula@protonmail.com>
2022-06-07 19:26:18 +0200

    move key related files to a feature branch

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import os
 from src.structure.functions import read_file
 from src.scale import Scale
 from src.chord import Chord
-from src.key import Key
 
 
 scales_content = read_file(os.path.join('config', 'scales.txt'))
 chords_content = read_file(os.path.join('config', 'chords.txt'))
-keys_content = read_file(os.path.join('config', 'keys.txt'))
 
 Scale.load_objects(scales_content)
 Chord.load_objects(chords_content)
