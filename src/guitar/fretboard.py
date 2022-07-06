937bbea41ded64fa3ac4d7c2af0bb06b458c8748
m-bakula <m.bakula@protonmail.com>
2022-07-07 00:51:50 +0200

    refactor and write docstrings for Fretboard and String classes

--- src/guitar/fretboard.py
+++ src/guitar/fretboard.py
@@ -11,32 +11,40 @@ class Fretboard:
         self.tuning: list[Note] = [a_note for a_note in tuning]
         self.strings_list: list[String] = []
 
-        self.update_fretboard(tuning, fretnum)
+        self.update_fretboard()
+
+    def update_fretboard(self):
+        """"Fills the list of strings according to tuning and number of frets"""
+        self.strings_list = [String(start_note, self.fretnum) for start_note in self.tuning]
 
     def get_tuning(self) -> list[Note]:
+        """Returns the tuning of the fretboard, starting from the first string"""
         return self.tuning
 
     def get_strings(self) -> list[String]:
+        """Returns the list of string objects in the fretboard"""
         return self.strings_list
 
     def get_str_num(self) -> int:
+        """Returns the number of strings of the fretboard"""
         return len(self.strings_list)
 
     def get_note_at(self, str_num: int, fret_num: int) -> Note:
+        """Returns the note at string str_num and fret fre_num"""
         return self.strings_list[str_num].get_note_at_fret(fret_num)
 
-    def update_fretboard(self, tuning: list[Note], fretnum: int):
-        self.strings_list = [String(start_note, fretnum) for start_note in tuning]
-
     def update_tuning(self, new_tuning: list[Note]):
+        """Replaces the list of strings using a new tuning"""
         self.tuning = new_tuning
-        self.update_fretboard(self.tuning, self.fretnum)
+        self.update_fretboard()
 
     def update_fretnum(self, new_fretnum: int):
+        """Replaces the list of strings using a new fret number"""
         self.fretnum = new_fretnum
-        self.update_fretboard(self.tuning, self.fretnum)
+        self.update_fretboard()
 
     def find_note(self, a_note: AbstractNote) -> list[tuple[int, list[int]]]:
+        """"Returns a list of tuples with numbers of strings and frets where a given note is located"""
         all_notes = []
 
         for str_num, a_string in enumerate(self.strings_list):
@@ -45,4 +53,5 @@ class Fretboard:
         return all_notes
 
     def find_notegroup(self, a_notegroup: NoteGroup):
+        """Returns find_note() for every note in a given notegroup"""
         pass
--- src/guitar/string.py
+++ src/guitar/string.py
@@ -10,22 +10,28 @@ class String:
         try:
             self.fretnum: int = fretnum
             self.fret_range: Iterable = range(self.fretnum + 1)
+            self.tuned_to: Note = tuned_to
             self.note_list: list[Note] = []
+            
             for a_fret in self.fret_range:
                 self.note_list.append(tuned_to.add_interval(a_fret))
         except ValueError as error:
             raise error
 
     def get_frets(self) -> int:
+        """Returns the number of frets for the string"""
         return self.fretnum
 
     def get_range(self) -> Iterable[int]:
+        """Returns a range object starting at 0 and ending at the last fret number"""
         return self.fret_range
 
     def get_notes(self) -> list[Note]:
+        """Returns all notes on the string, ordered by fret number"""
         return self.note_list
 
     def get_note_at_fret(self, fret_num: int) -> Note:
+        """Returns the note on the string at a given fret"""
         if fret_num not in self.fret_range:
             raise ValueError("Fret number can't be greater than the number of frets")
         else:
