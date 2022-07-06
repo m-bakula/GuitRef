4bee2703aeb27a2ebabb354ed017db05fa0255d5
m-bakula <m.bakula@protonmail.com>
2022-07-07 01:21:51 +0200

    stub out NoteGroupFrame and TuningFrame subclasses of tk.Frame

---
+++ src/gui/tuning_frame.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+from src.guitar.fretboard import Fretboard
+from src.guitar.string import String
+
+
+class TuningFrame(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        # TODO: implement
