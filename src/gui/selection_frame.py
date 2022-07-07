29f0739b8e603c4106ddaa44e015bdd33c41f763
m-bakula <m.bakula@protonmail.com>
2022-07-07 21:01:32 +0200

    prototype SelectionFrame

--- src/gui/selection_frame.py
+++ src/gui/selection_frame.py
@@ -1,23 +1,51 @@
 import tkinter as tk
 
-from src.notegroup import NoteGroup
+from src.structure.constants import NOTE_NAMES
+from src.chord import Chord
+from src.scale import Scale
+# to be deleted
+from src.structure.functions import read_file
+import os.path
 
 
-class NoteGroupFrame(tk.Frame):
+class SelectionFrame(tk.Frame):
     def __init__(self, *args, **kwargs) -> None:
+        Scale.load_recipes(read_file(os.path.join('config', 'scales.txt')))
         tk.Frame.__init__(self, *args, **kwargs)
-        self.selected_notegroup = None
-        self.drp_menu = None
+        self.selected_name = None
+        self.drop_name = None
+
+        self.selected_recipe = None
+        self.drop_recipe = None
+
         self.btn_show = None
         self.btn_hide = None
+        self.lbl_txt = None
 
         self.create_widgets()
         self.configure_grid()
 
     def create_widgets(self):
-        pass
-        # self.drp_menu = tk.OptionMenu(master=self, text='test')
+        name_list = NOTE_NAMES
+        var_name = tk.StringVar(self)
+        self.drop_name = tk.OptionMenu(self, var_name, *name_list)
+
+        recipes = [a_recipe[0] for a_recipe in Scale.get_recipes()]
+        var_rec = tk.StringVar(self)
+        self.drop_recipe = tk.OptionMenu(self, var_rec, *recipes)
 
     def configure_grid(self):
+        self.drop_name.grid(row=0, column=0)
+        self.drop_recipe.grid(row=0, column=1)
+
+    def select_name(self):
         pass
-        # self.drp_menu.grid(row=0, column=0)
+
+    def select_recipe(self):
+        pass
+
+    def show(self):
+        if self.selected_name is not None and self.selected_recipe is not None:
+            pass
+            # make selected object
+            # and highlight it
--- src/gui/window.py
+++ src/gui/window.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 
 from src.gui.fretboard_frame import FretboardFrame
+from src.gui.selection_frame import SelectionFrame
 from src.gui.toolbar_frame import ToolbarFrame
 
 
 class Window(tk.Tk):
-    def __init__(self,) -> None:
+    def __init__(self) -> None:
         tk.Tk.__init__(self)
         self.title('GuitRef')
         self.frm_gtr: FretboardFrame = FretboardFrame(master=self)
         self.frm_tlbr: ToolbarFrame = ToolbarFrame(master=self)
+        self.frm_slct: SelectionFrame = SelectionFrame(master=self)
 
         self.configure_grid()
         self.manage_frames()
 
     def configure_grid(self) -> None:
         self.columnconfigure(0, minsize=400, weight=1)
+        self.columnconfigure(1, minsize=200, weight=1)
         self.rowconfigure([0, 1], minsize=200, weight=1)
 
     def manage_frames(self) -> None:
         self.frm_gtr.grid(row=0, column=0,)
         self.frm_tlbr.grid(row=1, column=0)
+        self.frm_slct.grid(row=0, column=1)
