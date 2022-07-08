00fcae663b44bc36e1a258ccb72723b96482cd31
m-bakula <m.bakula@protonmail.com>
2022-07-08 15:28:05 +0200

    change data type for loaded_recipes to dict

--- src/loader.py
+++ src/loader.py
@@ -1,18 +1,17 @@
 class Loader:
     """Mix-in class for loading and unloading notegroup recipes from contents"""
-    loaded_recipes: set = set()
+    loaded_recipes: dict = {}
 
     def __init__(self, *args, **kwargs) -> None:
         """Loader doesn't have an __init__ of its own"""
         pass
 
     def __init_subclass__(cls) -> None:
-        cls.loaded_recipes = set()
+        cls.loaded_recipes = dict()
 
     @classmethod
     def load_recipes(cls, contents: list[str]) -> None:
-        """A generic factory for creating notegroup objects from recipe contents (e.g. text files). Adds created objects
-         to the loaded_recipes set"""
+        """Loads a dictionary of labels and recipes from contents (e.g. text files)"""
         for a_line in contents:
             # TODO: proper failsafe (regex) for not matching patterns
             if a_line[0] == '#' or a_line[0] == '\n':
@@ -20,8 +19,7 @@ class Loader:
             else:
                 label, *list_of_strings, = a_line.split()
                 arg = [int(a_string) for a_string in list_of_strings]
-                recipe_tuple = (label, tuple(arg))
-                cls.loaded_recipes.add(recipe_tuple)
+                cls.loaded_recipes[label] = arg
 
     @classmethod
     def clear_recipes(cls) -> None:
@@ -29,6 +27,6 @@ class Loader:
         cls.loaded_recipes.clear()
 
     @classmethod
-    def get_recipes(cls) -> set:
+    def get_recipes(cls) -> dict:
         """Returns the loaded_recipes set"""
         return cls.loaded_recipes
