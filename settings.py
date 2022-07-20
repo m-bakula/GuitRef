1d1d21cca99418156628ce8f57a7dc2b37ed9d8d
m-bakula <m.bakula@protonmail.com>
2022-07-20 21:17:22 +0200

    refactor constants in settings module

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ from src.structure.functions import read_file
 
 # define default values for settings
 DEFAULT_FILE = ''
-DEFAULT_BG = 'grey'
-DEFAULT_HL = 'red'
+DEFAULT_COLORS = {'fg': 'black', 'bg': 'grey', 'highl': 'red'}
+DEFAULT_FONT = {'font': 'Arial', 'big': 24, 'med_big': 20, 'med_small': 16, 'small': 12}
 DEFAULT_SCALES = read_file(os.path.join('config', 'scales.txt'))
 DEFAULT_CHORDS = read_file(os.path.join('config', 'chords.txt'))
 
@@ -16,15 +16,14 @@ class Settings:
                  file=DEFAULT_FILE,
                  scales=DEFAULT_SCALES,
                  chords=DEFAULT_CHORDS,
-                 background=DEFAULT_BG,
-                 highlight=DEFAULT_HL,
+                 colors=DEFAULT_COLORS,
+                 font=DEFAULT_FONT,
                  ) -> None:
         self.file = file
         self.scales = scales
         self.chords = chords
-        # colors
-        self.bg = background
-        self.hl = highlight
+        self.font = font
+        self.colors = colors
 
     def load_settings(self) -> None:
         pass
